61b1a8f2475902f70daad3d382295deb5d955614
poliferreyra <paoferreyra.fl@gmail.com>
2022-12-16 12:00:21 -0300

    Add value a todos los select y ocular vistas en JS

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-const $ = (selector)=>document.querySelector(selector)
+const $ = (selector) => document.querySelector(selector);
 
+// navBar
 const $btnHambur = $(".navbar-burger");
 const $navBarEnd = $(".navbar-menu");
+// botones del nav
+const $botonBalance = $("#boton-balance");
+const $botonCategoria = $("#boton-categoria");
+const $botonReporte = $("#boton-reporte");
+// secciones principales
+const $contenedorBalance = $("#contenedor-balance");
+const $contenedorReportes = $("#contenedor-reportes");
+const $contenedorCategoria = $("#contenedor-categoria");
+// nueva operacion
+const $botonNuevaOperacion = $("#boton-agregar-operacion");
+const $boxNuevaOperacion = $("#box-nueva-operacion");
+
+// FUNCIONES
+// navBar
+const toggleIsActive = () => {
+  $navBarEnd.classList.toggle("is-active");
+  $btnHambur.classList.toggle("is-active");
+};
+
+const nuevaOperacion = () => {}
 
 
+// secciones principales
+const vistaCategoria = () => {
+  $contenedorBalance.classList.add("is-hidden");
+  $contenedorReportes.classList.add("is-hidden");
+};
 
-const toggleIsActive = ()=>{
-    $navBarEnd.classList.toggle("is-active")
-    $btnHambur.classList.toggle("is-active");
-}
-
-// eventos
-
-$btnHambur.addEventListener("click",toggleIsActive)
-//
-
+// EVENTOS
+// menu hamburguesa
+$btnHambur.addEventListener("click", toggleIsActive);
+// boton categoria - ocultar otras vistas
+$botonCategoria.addEventListener("click", vistaCategoria);
+// vista nueva operacion
+$botonNuevaOperacion.addEventListener("click", nuevaOperacion);
