1055227f483a497f04f08e62e6bc4a692f00e72a
poliferreyra <paoferreyra.fl@gmail.com>
2022-12-28 20:31:36 -0300

    Add nueva operacion y localStorage

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // funcion maestra
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector); // pendiente optimizar vistas
 
 // ELEMENTOS DEL DOM
 // navBar
@@ -22,8 +23,21 @@ const $monto = $("#monto");
 const $tipoOperacion = $("#tipo-operacion");
 const $tipoCategoria = $("#tipo-categoria");
 const $fechaOperacion = $("#Fecha-operacion");
+// botones nueva operacion
 const $btnAgregarOperacion = $("#btnAgregarOperacion");
 const $btnCancelarOperacion = $("#btnCancelarOperacion");
+
+// array y objeto para nueva operacion
+const listaOperaciones = JSON.parse(localStorage.getItem("operacionNueva")) || [];
+console.log(listaOperaciones)
+
+const datosOperacion = {
+  descripcion:"",
+  monto:0,
+  tipo:"",
+  categoria:"",
+  fecha:""
+};
 
 // FUNCIONES
 // menu hamburguesa
@@ -61,6 +75,21 @@ const cerrarBoxOperacion = () => {
   $boxNuevaOperacion.classList.toggle("is-hidden");
   $contenedorBalance.classList.toggle("is-hidden");
 };
+// agregar nueva operacion
+const agregarOperaciones =()=>{
+let operacion = {...datosOperacion}
+
+operacion.descripcion = $descripcion.value
+operacion.monto = Number($monto.value)
+operacion.tipo = $tipoOperacion.value
+operacion.categoria = $tipoCategoria.value
+operacion.fecha = $fechaOperacion.value
+
+listaOperaciones.push(operacion)
+console.log(listaOperaciones)
+
+localStorage.setItem("operacionNueva", JSON.stringify(listaOperaciones))
+}
 
 // EVENTOS
 // menu hamburguesa
@@ -74,3 +103,5 @@ $botonReportes.addEventListener("click", vistaReportes);
 // vista seccion nueva operacion
 $botonNuevaOperacion.addEventListener("click", nuevaOperacion);
 $btnCancelarOperacion.addEventListener("click", cerrarBoxOperacion);
+// agregar nueva operacion
+$btnAgregarOperacion.addEventListener("click", agregarOperaciones)
